998bcf5127da6f04ec5f041fe623863d45f8b408
Sergey Stepanov <ser1653285@yandex.ru>
2022-12-14 16:29:57 +0300

    fix

--- src/components/ListOfOffers/Listing.js
+++ src/components/ListOfOffers/Listing.js
@@ -1,35 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
 
 function Listing({ items }) {
-
   return (
     <div className='item-list'>
       {items.map((item) => {
-        const quantityClass = `item-quantity level-${
-          item.quantity < 20 ? (item.quantity < 10 ? 'low' : 'medium') : 'high'
-        }`;
+        const quantityClass = classNames('item-quantity', {
+          'level-low': item.quantity < 10,
+          'level-medium': item.quantity >= 10 && item.quantity < 20,
+          'level-high': item.quantity >= 20,
+        });
+        const characterLimit =
+          item.title && item.title.length > 49
+            ? `${item.title.slice(0, 49)}...`
+            : item.title;
+        const currencySelection =
+          item.currency_code === 'USD'
+            ? '$'
+            : item.currency_code === 'EUR'
+            ? '€'
+            : `${item.currency_code} `;
+
         return item.error_messages ? null : (
           <div className='item' key={item.listing_id}>
             <div className='item-image'>
               <a href={item.url}>
-                <img
-                  src={item.MainImage && item.MainImage.url_570xN}
-                  alt=''
-                />
+                <img src={item.MainImage && item.MainImage.url_570xN} alt='' />
               </a>
             </div>
             <div className='item-details'>
-              <p className='item-title'>
-                {item.title && item.title.length > 49
-                  ? `${item.title.slice(0, 49)}...`
-                  : item.title}
-              </p>
+              <p className='item-title'>{characterLimit}</p>
               <p className='item-price'>
-                {item.currency_code === 'USD'
-                  ? '$'
-                  : item.currency_code === 'EUR'
-                  ? '€'
-                  : `${item.currency_code} `}
+                {currencySelection}
                 {item.price}
               </p>
               <p className={quantityClass}>{item.quantity} left</p>
